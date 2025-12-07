e7d35a0379a80e8a227e2be8cc278754ee59bd5b
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2025-12-07 12:07:50 -0300

    a

--- sonic-exe/title.gd
+++ sonic-exe/title.gd
@@ -1,14 +1,14 @@
 extends TileSetSource
 
 func _init():
-    var tile_set = TileSet.new()
-    
-    var texture = load("res://sonic-exe/assets/title_screen.png")
-    tile_set.create_tile(0)
-    tile_set.tile_set_texture(0, texture)
-    tile_set.tile_set_region(0, Rect2(Vector2(0, 0), texture.get_size()))
-    
-    self.tile_set = tile_set
+	var tile_set = TileSet.new()
+	
+	var texture = load("res://sonic-exe/assets/title_screen.png")
+	tile_set.create_tile(0)
+	tile_set.tile_set_texture(0, texture)
+	tile_set.tile_set_region(0, Rect2(Vector2(0, 0), texture.get_size()))
+	
+	self.tile_set = tile_set
 
 func get_tile_set() -> TileSet:
-    return self.tile_set
+	return self.tile_set
